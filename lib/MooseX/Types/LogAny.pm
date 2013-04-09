2ceb7608320b44c6f37a5615e73e728e14c98f38
Caleb Cushing <xenoterracide@gmail.com>
2013-04-09 12:00:50 -0500

    add implementation, documentation, and a test
    Signed-off-by: Caleb Cushing <xenoterracide@gmail.com>

--- lib/MooseX/Types/LogAny.pm
+++ lib/MooseX/Types/LogAny.pm
@@ -5,6 +5,61 @@ use warnings;
 
 # VERSION
 
+use MooseX::Types -declare => [qw(
+	LogAny
+	LogAnyBase
+	LogAnyNull
+)];
+
+class_type LogAnyBase, { class => 'Log::Any::Adapter::Base' };
+class_type LogAnyNull, { class => 'Log::Any::Adapter::Null' };
+subtype    LogAny, as LogAnyBase|LogAnyNull;
+
 1;
 
-# ABSTRACT: MooseX::Types::LogAny
+# ABSTRACT: Moose Types for Log::Any
+
+=head1 SYNOPSIS
+
+	use Moose;
+	use Class::Load 0.20 'load_class';
+	use MooseX::Types::LogAny qw( LogAny );
+
+	has log => (
+		isa     => LogAny,
+		is      => 'ro',
+		lazy    => 1,
+		default => sub { load_class('Log::Any')->get_logger },
+	);
+
+=head1 DESCRIPTION
+
+The motivation behind this modules is that L<Log::Any::Adapter::Null> does not
+inherit from L<Log::Any::Adapter::Base> which all of the other adapters do.
+Moose will not allow a type union of undefined types unless they are Moose
+classes. This module allows you to simply use the L<LogAny> type which has
+done the union for you.
+
+=sub LogAny
+
+Type union of L<LogAnyNull> and L<LogAnyBase>
+
+=sub LogAnyNull
+
+Class L<Log::Any::Adapter::Null>
+
+=sub LogAnyBase
+
+Class L<Log::Any::Adapter::Base>
+
+=head1 SEE ALSO
+
+=over
+
+=item L<MooseX::Types>
+
+=item L<Log::Any>
+
+=item L<Log::Any::Adapter>
+
+=back
---
+++ t/basic.t
@@ -0,0 +1,33 @@
+use strict;
+use warnings;
+use Test::More;
+use Class::Load 'load_class';
+use Test::Fatal;
+
+{
+	package Test;
+	use Moose;
+	use Class::Load 'load_class';
+	use MooseX::Types::LogAny qw( LogAny );
+
+	has log => (
+		isa => LogAny,
+		is  => 'rw',
+	);
+}
+
+my $t = new_ok( load_class('Test') );
+
+load_class('Log::Any::Adapter')->set('Null');
+
+my $e0 = exception { $t->log( load_class('Log::Any')->get_logger ) };
+
+ok ! $e0, 'using null logger does not throw an exception';
+
+load_class('Log::Any::Adapter')->set('Stderr');
+
+my $e1 = exception { $t->log( load_class('Log::Any')->get_logger ) };
+
+ok ! $e1, 'using sterr logger does not throw an exception';
+
+done_testing;
